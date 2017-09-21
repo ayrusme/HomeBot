2b84aa7b972453bd4c6ab44cd9d47980e04ae58a
Surya Raman <suryaramans@visualbi.com>
2017-09-21 14:26:21 +0530

    Revert "Added UI and moved Backend into Backend folder. Fixed requirements"
    This reverts commit f901fccc15e3140026ccc8264ff3e7a5d9d403cd.

---
+++ src/server.py
@@ -0,0 +1,51 @@
+"""The Server file for Home Bot"""
+
+from flask import Flask, jsonify, render_template, request
+
+import codes
+import football_features as football
+
+APP = Flask(__name__)
+
+@APP.route("/", methods=['GET'])
+def homepage():
+    """Returns the homepage
+    Returns
+    ------
+    homepage.html : html page
+        contains information to be displated as the homepage
+
+    """
+    return render_template('homepage.html')
+
+@APP.route("/football/")
+def send_available_endpoints():
+    return ("""
+              /football/PL/ : For information about Premier League &nbsp
+              /football/PL/leaguetable/ : For the current Premier League table &nbsp
+              /football/PL/fixtures/ : For current GW fixtures &nbsp
+              /football/PL/fixtures/all/ : For fixtures for all GW's &nbsp
+              /football/PL/results/ : For results in the current season &nbsp
+              /football/PL/live/ : For live match scores   
+            """)
+
+@APP.route("/football/PL/")
+def send_league_data():
+    teams = football.send_league_data()
+    return render_template('league_table.html',teams=teams)
+
+@APP.route("/football/PL/leaguetable/")
+def get_league_table():
+    return football.get_league_table()
+
+@APP.route("/football/PL/fixtures")
+def get_fixtures():
+    return football.get_league_fixtures(codes.CURRENT_GW)
+
+if __name__ == "__main__":
+    APP.run(
+        '0.0.0.0',
+        8080,
+        debug=True,
+        threaded=True
+        )
