492cd0b5f081d7b354f909ba7ad33541a4ddcec5
Surya Raman <suryaramans@visualbi.com>
2017-09-21 14:27:44 +0530

    Added missing files after reversing last commit. Backend is moved to backend and UI in UI folder.

---
+++ Backend/src/codes.py
@@ -0,0 +1,8 @@
+PL_ENDPOINT = "http://api.football-data.org/v1/soccerseasons/445"
+CURRENT_GW = "current"
+ALL_GW = "all gameweeks"
+RESULTS = "results"
+SCHEDULED = "SCHEDULED"
+TIMED = "TIMED"
+FINISHED = "FINISHED"
+LIVE = "IN_PLAY"
